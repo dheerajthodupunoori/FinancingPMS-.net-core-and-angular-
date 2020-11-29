4f94e320f71fd9bafbb0f3b8869c6576ed20c8fc
Dheeraj Thodupunuri <dheeraj.thodupunuri@factset.com>
2020-11-29 14:37:36 +0530

    Added xunit test project and excluded bin and obj folders from test project

---
+++ FinancingPMSSolution/FinancingPMS.Test/Controller/UnitTest1.cs
@@ -0,0 +1,24 @@
+using FinancingPMS.Controllers;
+using FinancingPMS.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using Xunit;
+
+namespace FinancingPMS.Test
+{
+    public class FirmControllerTest
+    {
+        private readonly IFirmService _firmService;
+        private readonly ILogger _logger;
+
+        public FirmControllerTest()
+        {
+        }
+
+        [Fact]
+        public void GetAllFirmsTest()
+        {
+            Assert.Equal("true", "true");
+        }
+    }
+}
